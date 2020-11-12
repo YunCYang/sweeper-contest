6e31b771410798b0b76578dcf9672fc3545242b6
YunCYang <ericeorl2013@gmail.com>
2020-11-11 23:49:55 -0800

    add index.js file

---
+++ server/index.js
@@ -0,0 +1,50 @@
+require('dotenv/config');
+const express = require('express');
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+// const nodemailer = require('nodemailer');
+
+// const db = require('./database');
+const ClientError = require('./client-error');
+const staticMiddleware = require('./static-middleware');
+const sessionMiddleware = require('./session-middleware');
+// const emailTemplate = require('./email');
+
+const app = express();
+
+app.use(staticMiddleware);
+app.use(sessionMiddleware);
+
+app.use(express.json());
+
+// const intTest = (id, next) => {
+//   const test = /^[0-9]\d*$/;
+//   if (!test.exec(id)) {
+//     return next(new ClientError(`id ${id} is not a valid positive integer`, 400));
+//   } else return null;
+// };
+
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: process.env.EMAIL_LOGIN,
+//     pass: process.env.EMAIL_PASSWORD
+//   }
+// });
+
+app.use('/api', (req, res, next) => {
+  next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
+});
+
+app.use((err, req, res, next) => {
+  if (err instanceof ClientError) {
+    res.status(err.status).json({ error: err.message, status: err.status });
+  } else {
+    console.error(err);
+    res.status(500).json({
+      error: 'an unexpected error occurred'
+    });
+  }
+});
+
+module.exports = app;
